e282c28722844268f8e6d1097b9c78cd999838cc
Motacilla <vicente.jimenez.ontiveros@gmail.com>
2026-05-25 10:55:56 +0200

    corrected the approximation to the new equation 6 in the main text

--- R/expected_A_empirical.R
+++ R/expected_A_empirical.R
@@ -1,14 +1,15 @@
 expected_A_empirical <- function(p, q, S, N) {
-  pq <- outer(p, q, "*")       
-  m  <- rowMeans((1 - pq)^N)   
+  pq <- outer((1 - p), q * N, "^")       
+  m  <- rowMeans(pq)   
   (S - 1) * (1 - m)
 }
+
 
 bootstrap_A_empirical <- function(n_boot, p, q, S, N){
   bootstrap_values <- matrix(NA, nrow = n_boot, ncol = length(p))
   for(b in 1:n_boot){
     q_boot <- sample(q, size = S, replace = TRUE)
-    bootstrap_values[b, ] <- expected_A_empirical(p, q, S, N)
+    bootstrap_values[b, ] <- expected_A_empirical(p, q_boot, S, N)
   }
   bootstrap_values
 }
